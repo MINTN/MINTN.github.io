baf4a7f4df40e8fd47e08fbddd3d2181cc1851a6
MINTN <51085225+MINTN@users.noreply.github.com>
2023-09-09 02:00:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ tg.MainButton.text = "Авторизоваться";
 function themeChanger() {
     tg.MainButton.show();
     tg.MainButton.onClick(function send() {
-        alert("knopka pressed");
         document.querySelector('form').addEventListener('submit', function(event) {
             event.preventDefault();
             var text = document.getElementById('username').value;
+            alert(text)
             var password = document.getElementById('password').value;
             let kek = '{\n\t"username": "' + text + '",\n\t"userid": "' + password + '"\n}';
             alert(kek);
