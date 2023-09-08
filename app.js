71c7bf60ad6b1d4b5cf1d7b708136e8fb33d0b27
MINTN <51085225+MINTN@users.noreply.github.com>
2023-09-08 23:56:46 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let tg = window.Telegram.WebApp
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF"
+tg.MainButton.Color = "#2cab37"
+
+let btn1 = document.getElementById("btn1")
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    tg.sendData("1");
+})
+
+let = usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.inerText = `&{tg.initDataUnsafe.user.id}`
+
+usercard.appendChild(p)
