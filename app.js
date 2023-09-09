1f2963e490bfbd9e53220811f45ab65703f9a4f7
MINTN <51085225+MINTN@users.noreply.github.com>
2023-09-09 03:29:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function themeChanger() {
     tg.MainButton.onClick(function send() {
         let login = document.getElementById('username').value;
         let pass = document.getElementById('password').value;
-        let kek = login + "|" pass;
+        let kek = login + "|" + pass;
         tg.sendData(kek);
 })
 }
