fed3b3226af03ca21f8353362647e335ed4643c0
MINTN <51085225+MINTN@users.noreply.github.com>
2023-10-02 13:04:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-let tg = window.Telegram.WebApp;
-
-tg.expand();
-
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.Color = "#33cc18";
-tg.MainButton.text = "Авторизоваться";
-function themeChanger() {
-    tg.MainButton.show();
-    tg.MainButton.onClick(function send() {
-        let login = document.getElementById('username').value;
-        let pass = document.getElementById('password').value;
-        let kek = login + " " + pass;
-        tg.sendData(kek);
-})
-}
-
-let = usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.id}`
-
-usercard.appendChild(p);
+function start_func() {
+  let telegramApp = window.Telegram.WebApp;
+  document.querySelector('body').style.backgroundColor = telegramApp.themeParams.secondary_bg_color;
+  telegramApp.expand();
+  telegramApp.MainButton.text = "TestMainButton";
+  telegramApp.MainButton.color = "#ff0000";
+  telegramApp.MainButton.show();
+  telegramApp.MainButton.onClick(function send() {
+    let tgusername = telegramApp.initData;
+    let tguserId = "1231313";
+    let login = document.getElementById('username').value;
+    let pass = document.getElementById('password').value;
+    //let kek = '{\n\t"tgusername": "' + tgusername + '",\n\t"tguserId": "' + tguserId + '",\n\t"login": "' + login + '",\n\t"pass": "' + pass + '"\n}';
+    let kek = '{\n\t"login": "' + login + '",\n\t"pass": "' + pass + '"\n}';
+    telegramApp.sendData(kek);
+  })
+};
